d4a4aed0abdbc6a14eca08030f6dc855c19ea8d6
Daniel Flores Hurtado <daniel.hurtado4774@gmail.com>
2023-07-06 17:49:04 -0500

    Attempt to update Angular so compatibile with ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { StoreModule } from '@ngrx/store';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
